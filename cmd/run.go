d70bcbf1c268ad13945062d1962c9fa1b37e5109
Carlos Estrada <carlos.deg02@hotmail.com>
2024-07-15 18:45:51 -0700

    Update run.go to instantiate updater factory and impl

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Loptt/dns-updater/updater"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	uf := &updater.UpdaterDuckDNSFactory{}
+
+	domain := "lopttus"
+	u, err := uf.CreateUpdater(domain)
+	if err != nil {
+		fmt.Printf("Failed to create updater with error: %v\n", err)
+	}
+
+	if err := u.Update(); err != nil {
+		fmt.Printf("Failed to update with error: %v\n", err)
+	}
+
+	fmt.Printf("Update successful\n")
 }
