34c0f29db3c934f5fab04776741d6d738cacda39
Carlos Estrada <carlos.deg02@hotmail.com>
2024-07-21 19:52:32 -0700

    Add scheduler struct and Run function

--- cmd/run.go
+++ cmd/run.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/Loptt/dns-updater/scheduler"
 	"github.com/Loptt/dns-updater/updater"
 )
 
@@ -11,14 +13,13 @@ func main() {
 
 	domain := "lopttus"
 	token := "test_token"
+	interval := 1 * time.Second
 	u, err := uf.CreateUpdater(domain, token)
 	if err != nil {
 		fmt.Printf("Failed to create updater with error: %v\n", err)
 	}
 
-	if err := u.Update(); err != nil {
-		fmt.Printf("Failed to update with error: %v\n", err)
-	}
+	s := scheduler.NewScheduler(interval, u)
 
-	fmt.Printf("Update successful\n")
+	s.Run()
 }
---
+++ scheduler/scheduler.go
@@ -0,0 +1,27 @@
+package scheduler
+
+import (
+	"log"
+	"time"
+
+	"github.com/Loptt/dns-updater/updater"
+)
+
+type Scheduler struct {
+	interval time.Duration
+	ui       updater.UpdaterInterface
+}
+
+func (s *Scheduler) Run() {
+	runTicker := time.NewTicker(s.interval)
+
+	for range runTicker.C {
+		if err := s.ui.Update(); err != nil {
+			log.Printf("Error: failed to update record: %v\n", err)
+		}
+	}
+}
+
+func NewScheduler(interval time.Duration, ui updater.UpdaterInterface) *Scheduler {
+	return &Scheduler{interval: interval, ui: ui}
+}
