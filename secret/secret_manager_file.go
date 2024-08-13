fb9c95b8e5e1408f11c8659e09e512b50e2061ea
Carlos Estrada <carlos.deg02@hotmail.com>
2024-08-12 19:22:11 -0700

    Add secret manager interface and file impl

---
+++ secret/secret_manager_file.go
@@ -0,0 +1,23 @@
+package secret
+
+import (
+	"fmt"
+
+	"github.com/Loptt/dns-updater/file"
+)
+
+// SecretManagerFile implements the SecretManagerInterface to fetch secrets
+// from the file system.
+type SecretManagerFile struct {
+	f file.FileManagerInterface
+}
+
+// Fetch takes the key as the file path, and returns its contents.
+func (sm *SecretManagerFile) Fetch(key string) (string, error) {
+	result, err := sm.f.Read(key)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch secret from file %s with error: %v", key, err)
+	}
+
+	return result, nil
+}
